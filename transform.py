d68a4e03ec8f56ca3c77fa3897698a58dd126c3b
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-12-10 20:02:35 +0100

    add transforms

---
+++ transform.py
@@ -0,0 +1,46 @@
+import torch
+from utils.table import canmergetensor as canmerge
+from utils.table import mergetensor as mergetensor
+
+def compose(transforms):
+    assert isinstance(transforms, list)
+    for tr in transforms:
+        assert callable(tr), 'dict of functions expected'
+    def composition(z):
+        for tr in transforms:
+            z = tr(z)
+        return z
+    return composition
+
+def tablemergekeys():
+    def mergekeys(tbl):
+        mergetbl = {}
+        if isinstance(tbl, dict):
+            for idx, elem in tbl.items():
+                for key, value in elem.items():
+                    if not key in mergetbl:
+                        mergetbl[key] = {}
+                    mergetbl[key][idx] = value
+        elif isinstance(tbl, list):
+            for elem in tbl:
+                for key, value in elem.items():
+                    if not key in mergetbl:
+                        mergetbl[key] = []
+                    mergetbl[key].append(value)
+        return mergetbl
+    return mergekeys
+
+tableapply = lambda f: lambda d: dict(map(lambda (k,v): (k, f(v)), d.iteritems()))
+
+def makebatch(merge = None):
+    if merge:
+        makebatch = compose([tablemergekeys(), merge])
+    else:
+        makebatch = compose([
+            tablemergekeys(),
+            tableapply(lambda field: canmerge(field) and mergetensor(field) or field)
+            ])
+
+    return lambda samples: makebatch(samples)
+
+
---
+++ utils/table.py
@@ -0,0 +1,20 @@
+import torch
+
+def canmergetensor(tbl):
+    if not isinstance(tbl, list):
+        return False
+
+    if torch.is_tensor(tbl[0]):
+        sz = tbl[0].numel()
+        for v in tbl:
+            if v.numel() != sz:
+                return False
+        return True
+    return False
+
+def mergetensor(tbl):
+    sz = [len(tbl)] + list(tbl[0].size())
+    res = tbl[0].new(torch.Size(sz))
+    for i,v in enumerate(tbl):
+        res[i].copy_(v)
+    return res
