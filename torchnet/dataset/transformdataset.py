a911d132d94088981618817b58f57e258a8911df
Sergey Zagoruyko <zagoruyko2@gmail.com>
2016-12-21 13:18:47 +0100

    single function transform

--- torchnet/dataset/transformdataset.py
+++ torchnet/dataset/transformdataset.py
@@ -3,11 +3,13 @@ import torch
 
 class TransformDataset(dataset.Dataset):
     def __init__(self, dataset, transforms):
-        assert isinstance(transforms, dict), 'expected a dict of transforms'
-        for k,v in transforms.items():
-            assert callable(v), str(k) + ' is not a function'
+        assert isinstance(transforms, dict) or callable(transforms), \
+                'expected a dict of transforms or a function'
+        if isinstance(transforms, dict):
+            for k,v in transforms.items():
+                assert callable(v), str(k) + ' is not a function'
         self.dataset = dataset
-        self.transforms = transforms.copy()
+        self.transforms = transforms
 
     def __len__(self):
         return len(self.dataset)
@@ -15,7 +17,10 @@ class TransformDataset(dataset.Dataset):
     def __getitem__(self, idx):
         super(TransformDataset, self).__getitem__(idx)
         z = self.dataset[idx]
-        for k, transform in self.transforms.items():
-            z[k] = transform(z[k])
+        if isinstance(self.transforms, dict):
+            for k, transform in self.transforms.items():
+                z[k] = transform(z[k])
+        else:
+            z = self.transforms(z)
         return z
 
