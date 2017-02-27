8fafb5520d624fe182267fc82f600c5cca44cfdc
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2017-02-27 16:23:29 +0530

    Make load optional in ListDataset

--- torchnet/dataset/listdataset.py
+++ torchnet/dataset/listdataset.py
@@ -6,9 +6,9 @@ class ListDataset(Dataset):
     """
     Dataset which loads data from a list using given function.
 
-    Considering a `elem_list` (can be a `string`, `torch.LongTensor`, or any other
-    iterable) i-th sample of a dataset will be returned by `load(elem_list[i])`,
-    where `load()` is a function provided by the user.
+    Considering a `elem_list` (can be a `string`, `torch.LongTensor`, or any
+    other iterable) i-th sample of a dataset will be returned by
+    `load(elem_list[i])`, where `load()` is a function provided by the user.
 
     If `path` is provided, `elem_list` is assumed to be a list of strings, and
     each element `elem_list[i]` will prefixed by `path/` when fed to `load()`.
@@ -21,15 +21,16 @@ class ListDataset(Dataset):
         elem_list (iterable/str): List of arguments which will be passed to
             `load` function. It can also be a path to file with each line
             containing the arguments to `load`
-        load (function): Function which loads the data. i-th sample is returned
-            by `load(elem_list[i])`.
+        load (function, optional): Function which loads the data.
+            i-th sample is returned by `load(elem_list[i])`. By default `load`
+            is identity i.e, `lambda x: x`
         path (str, optional): Defaults to None. If a string is provided,
             `elem_list` is assumed to be a list of strings, and each element
             `elem_list[i]` will prefixed by this string when fed to `load()`.
 
     """
 
-    def __init__(self, elem_list, load, path=None):
+    def __init__(self, elem_list, load=lambda x: x, path=None):
         super(ListDataset, self).__init__()
 
         if isinstance(elem_list, str):
