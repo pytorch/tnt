df0af10474c56935677fc852ab9fe7c6e263128e
Xu Kui <xukui@sensetime.com>
2017-12-14 15:44:08 +0800

    provide more easier way to get mean, std. And also provide current value

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import shutil
 import sys
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 long_description = "an abstraction to train neural networks"
 
@@ -12,9 +12,9 @@ setup_info = dict(
     # Metadata
     name='torchnet',
     version=VERSION,
-    author='Sergey Zagoruyko',
-    author_email='sergey.zagoruyko@enpc.fr',
-    url='https://github.com/pytorch/tnt',
+    author='Barry Kui',
+    author_email='xukui.cs@gmail.com',
+    url='https://github.com/barrykui/tnt',
     description='an abstraction to train neural networks',
     long_description=long_description,
     license='BSD',
