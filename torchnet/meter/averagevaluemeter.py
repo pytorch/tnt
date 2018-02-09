Correcting averagevaluemeter when n>1 (#75)
* Correcting averagevaluemeter when n>1

* Correcting lint

torchnet/meter/averagevaluemeter.py
@@ -22,7 +22,7 @@ class AverageValueMeter(meter.Meter):
             self.mean_old = self.mean
             self.m_s = 0.0
         else:
-            self.mean = self.mean_old + (value - self.mean_old) / float(self.n)
+            self.mean = self.mean_old + (value - n * self.mean_old) / float(self.n)
             self.m_s += (value - self.mean_old) * (value - self.mean)
             self.mean_old = self.mean
             self.std = math.sqrt(self.m_s / (self.n - 1.0))
@@ -36,6 +36,6 @@ class AverageValueMeter(meter.Meter):
         self.var = 0.0
         self.val = 0.0
         self.mean = np.nan
-        self.mean_old = self.mean
+        self.mean_old = 0.0
         self.m_s = 0.0
         self.std = np.nan