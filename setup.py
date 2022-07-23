54b61a47035b947bd8e67c952af08e610bbf316d
Danielle Pintz <daniellepintz@fb.com>
2022-07-23 16:26:57 -0700

    Update setup.py
    Reviewed By: ananthsub
    
    Differential Revision: D38100883
    
    fbshipit-source-id: 2296408d68a286c4b4ee3dba3561ec1fd7814325

--- setup.py
+++ setup.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python
-import os
-import shutil
-import sys
-
 from setuptools import find_packages, setup
 
 VERSION = "0.0.5.1"
 
-long_description = "Simple tools for logging and visualizing, loading and training"
-
-setup_info = dict(
+setup(
     # Metadata
-    name="torchnet",
+    name="torchtnt",
     version=VERSION,
     author="PyTorch",
-    author_email="sergey.zagoruyko@enpc.fr",
+    author_email="daniellepintz@fb.com",
     url="https://github.com/pytorch/tnt/",
-    description="an abstraction to train neural networks",
-    long_description=long_description,
+    description="A lightweight library for PyTorch training tools and utilities",
     license="BSD",
     # Package info
     packages=find_packages(exclude=("test", "docs")),
     zip_safe=True,
     install_requires=["torch", "six", "future", "visdom"],
 )
-
-setup(**setup_info)
