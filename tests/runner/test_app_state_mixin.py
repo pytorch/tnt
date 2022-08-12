511f79968bc1814020987feb80b189b583d125fa
Danielle Pintz <daniellepintz@fb.com>
2022-08-12 13:48:40 -0700

    Add _AppStateMixin and _OnExceptionMixin
    Reviewed By: ananthsub
    
    Differential Revision: D38412615
    
    fbshipit-source-id: 54ea41e9a772a2238cac43319f0939dbd5057905

---
+++ tests/runner/test_app_state_mixin.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+import torch
+from torch import nn
+from torchtnt.runner.unit import _AppStateMixin
+
+
+class AppStateMixinTest(unittest.TestCase):
+    def test_tracked_modules(self) -> None:
+        """
+        Test setting, getting, and deleting tracked_modules
+        """
+
+        class MyUnit(_AppStateMixin):
+            def __init__(self):
+                super().__init__()
+                self.module_a = nn.Linear(1, 1)
+                self.loss_fn_b = nn.CrossEntropyLoss()
+
+        my_unit = MyUnit()
+
+        # assert that the attributes are stored in tracked_modules
+        self.assertEqual(my_unit.tracked_modules()["module_a"], my_unit.module_a)
+        self.assertEqual(my_unit.tracked_modules()["loss_fn_b"], my_unit.loss_fn_b)
+
+        # delete the attributes
+        my_unit.module_a = None
+        my_unit.loss_fn_b = None
+
+        # the attributes should be removed from tracked_modules
+        self.assertFalse("module_a" in my_unit.tracked_modules())
+        self.assertFalse("loss_fn_b" in my_unit.tracked_modules())
+
+    def test_tracked_optimizers(self) -> None:
+        """
+        Test setting, getting, and deleting tracked_optimizers
+        """
+
+        class MyUnit(_AppStateMixin):
+            def __init__(self):
+                super().__init__()
+                module_a = nn.Linear(1, 1)
+                self.optimizer_c = torch.optim.SGD(module_a.parameters(), lr=0.1)
+
+        my_unit = MyUnit()
+
+        # assert that the attribute is stored in tracked_optimizers
+        self.assertEqual(
+            my_unit.tracked_optimizers()["optimizer_c"], my_unit.optimizer_c
+        )
+
+        # delete the attribute
+        my_unit.optimizer_c = None
+
+        # the attribute should be removed from tracked_optimizers
+        self.assertFalse("optimizer_c" in my_unit.tracked_optimizers())
+
+    def test_tracked_lr_schedulers(self) -> None:
+        """
+        Test setting, getting, and deleting tracked_lr_schedulers
+        """
+
+        class MyUnit(_AppStateMixin):
+            def __init__(self):
+                super().__init__()
+                module_a = nn.Linear(1, 1)
+                optimizer_b = torch.optim.SGD(module_a.parameters(), lr=0.1)
+                self.lr_scheduler_d = torch.optim.lr_scheduler.StepLR(
+                    optimizer_b, step_size=30, gamma=0.1
+                )
+
+        my_unit = MyUnit()
+
+        # assert that the attribute is stored in tracked_lr_schedulers
+        self.assertEqual(
+            my_unit.tracked_lr_schedulers()["lr_scheduler_d"], my_unit.lr_scheduler_d
+        )
+
+        # delete the attribute
+        my_unit.lr_scheduler_d = None
+
+        # the attribute should be removed from tracked_lr_schedulers
+        self.assertFalse("lr_scheduler_d" in my_unit.tracked_lr_schedulers())
+
+    def test_app_state(self) -> None:
+        """
+        Test the app_state method
+        """
+
+        class MyUnit(_AppStateMixin):
+            def __init__(self):
+                super().__init__()
+                self.module_a = nn.Linear(1, 1)
+                self.loss_fn_b = nn.CrossEntropyLoss()
+                self.optimizer_c = torch.optim.SGD(self.module_a.parameters(), lr=0.1)
+                self.lr_scheduler_d = torch.optim.lr_scheduler.StepLR(
+                    self.optimizer_c, step_size=30, gamma=0.1
+                )
+
+        my_unit = MyUnit()
+
+        # the attributes should be in app_state
+        for key in ("module_a", "loss_fn_b", "optimizer_c", "lr_scheduler_d"):
+            self.assertTrue(key in my_unit.app_state())
+
+        # delete the attributes
+        my_unit.module_a = None
+        my_unit.loss_fn_b = None
+        my_unit.optimizer_c = None
+        my_unit.lr_scheduler_d = None
+
+        # the attributes should no longer be in app_state
+        for key in ("module_a", "loss_fn_b", "optimizer_c", "lr_scheduler_d"):
+            self.assertFalse(key in my_unit.app_state())
