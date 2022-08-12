5e559973d7adb56e0e8fa9bdf143d45ddee392cb
Danielle Pintz <daniellepintz@fb.com>
2022-08-12 13:48:15 -0700

    Add unit tests for progress
    Differential Revision: D38632042
    
    fbshipit-source-id: 725b5caff3e9b400a1ec27f842cd181927eee07e

---
+++ tests/runner/test_progress.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+from torchtnt.runner.progress import Progress
+
+
+class ProgressTest(unittest.TestCase):
+    def test_progress_reset(self) -> None:
+        """
+        Test the reset method of Progress
+        """
+        progress = Progress(
+            num_epochs_completed=2,
+            num_steps_completed=8,
+            num_steps_completed_in_epoch=4,
+        )
+
+        self.assertEqual(progress.num_epochs_completed, 2)
+        self.assertEqual(progress.num_steps_completed, 8)
+        self.assertEqual(progress.num_steps_completed_in_epoch, 4)
+
+        progress.reset()
+
+        self.assertEqual(progress.num_epochs_completed, 0)
+        self.assertEqual(progress.num_steps_completed, 0)
+        self.assertEqual(progress.num_steps_completed_in_epoch, 0)
+
+    def test_progress_state_dict(self) -> None:
+        """
+        Test the state_dict and load_state_dict methods of Progress
+        """
+        progress = Progress(
+            num_epochs_completed=2,
+            num_steps_completed=8,
+            num_steps_completed_in_epoch=4,
+        )
+
+        state_dict = progress.state_dict()
+
+        new_progress = Progress()
+
+        self.assertEqual(new_progress.num_epochs_completed, 0)
+        self.assertEqual(new_progress.num_steps_completed, 0)
+        self.assertEqual(new_progress.num_steps_completed_in_epoch, 0)
+
+        new_progress.load_state_dict(state_dict)
+
+        self.assertEqual(new_progress.num_epochs_completed, 2)
+        self.assertEqual(new_progress.num_steps_completed, 8)
+        self.assertEqual(new_progress.num_steps_completed_in_epoch, 4)
