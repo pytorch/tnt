1ef41504e96872f0b6c89282d9fa6760313080b0
Danielle Pintz <daniellepintz@fb.com>
2022-08-12 13:49:09 -0700

    Add runner.state
    Reviewed By: ananthsub
    
    Differential Revision: D38566594
    
    fbshipit-source-id: aacfd2c5edf386a14468368175d2d293aa101834

---
+++ torchtnt/runner/state.py
@@ -0,0 +1,45 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from dataclasses import dataclass
+from typing import Any, Iterable, Optional
+
+from torchtnt.runner.progress import Progress
+from typing_extensions import Literal
+
+
+@dataclass
+class PhaseState:
+    """State for each phase (train, eval, predict)"""
+
+    progress: Progress
+
+    # input arguments
+    # pyre-ignore: Invalid type variable [34]
+    dataloader: Iterable[Any]
+    max_epochs: Optional[int] = None  # used only for train
+    max_steps_per_epoch: Optional[int] = None
+    evaluate_every_n_steps: Optional[int] = None  # used only for evaluate
+    evaluate_every_n_epochs: Optional[int] = None  # used only for evaluate
+
+    # contains the output from the last call to the user's `*_step` method
+    # pyre-ignore: Invalid type variable [34]
+    step_output: Any = None
+
+
+@dataclass
+class State:
+    """Parent State class which can contain up to 3 instances of PhaseState, for the 3 phases.
+
+    A new State class is created (and the previous one erased) each time an entry point is called.
+    """
+
+    entry_point: Literal["fit", "train", "evaluate", "predict"]
+
+    train_state: Optional[PhaseState] = None
+    eval_state: Optional[PhaseState] = None
+    predict_state: Optional[PhaseState] = None
