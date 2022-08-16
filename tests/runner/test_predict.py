b86675077a6ee52c67b7ea0b116c4195fae57731
Danielle Pintz <daniellepintz@fb.com>
2022-08-16 11:02:20 -0700

    Add runner.predict
    Differential Revision: D38692009
    
    fbshipit-source-id: ebd98eb599d58e8bf860e0736594749639186227

---
+++ tests/runner/test_predict.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+from torchtnt.runner.predict import predict
+
+from torchtnt.tests.runner.utils import DummyPredictUnit, generate_random_dataloader
+
+
+class PredictTest(unittest.TestCase):
+    def test_predict(self) -> None:
+        """
+        Test predict entry point
+        """
+        input_dim = 2
+        dataset_len = 10
+        batch_size = 2
+        expected_steps = dataset_len / batch_size
+
+        my_unit = DummyPredictUnit(input_dim=input_dim)
+        initial_training_mode = my_unit.module.training
+
+        dataloader = generate_random_dataloader(dataset_len, input_dim, batch_size)
+
+        state = predict(my_unit, dataloader)
+
+        self.assertEqual(state.predict_state.progress.num_epochs_completed, 1)
+        self.assertEqual(state.predict_state.progress.num_steps_completed_in_epoch, 0)
+        self.assertEqual(
+            state.predict_state.progress.num_steps_completed, expected_steps
+        )
+
+        # step_output should be reset to None
+        self.assertEqual(state.predict_state.step_output, None)
+
+        self.assertEqual(my_unit.module.training, initial_training_mode)
+
+    def test_predict_max_steps_per_epoch(self) -> None:
+        """
+        Test predict entry point with max_steps_per_epoch
+        """
+        input_dim = 2
+        dataset_len = 10
+        batch_size = 2
+        max_steps_per_epoch = 3
+
+        my_unit = DummyPredictUnit(input_dim=input_dim)
+        initial_training_mode = my_unit.module.training
+
+        dataloader = generate_random_dataloader(dataset_len, input_dim, batch_size)
+
+        state = predict(my_unit, dataloader, max_steps_per_epoch=max_steps_per_epoch)
+
+        self.assertEqual(state.predict_state.progress.num_epochs_completed, 1)
+        self.assertEqual(state.predict_state.progress.num_steps_completed_in_epoch, 0)
+        self.assertEqual(
+            state.predict_state.progress.num_steps_completed, max_steps_per_epoch
+        )
+
+        # step_output should be reset to None
+        self.assertEqual(state.predict_state.step_output, None)
+
+        self.assertEqual(my_unit.module.training, initial_training_mode)
---
+++ torchtnt/runner/predict.py
@@ -0,0 +1,97 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+from typing import Iterable, Optional
+
+import torch
+
+from torchtnt.runner.progress import Progress
+from torchtnt.runner.state import EntryPoint, PhaseState, State
+from torchtnt.runner.unit import PredictUnit, TPredictData
+from torchtnt.runner.utils import (
+    _check_loop_condition,
+    _is_epoch_done,
+    _reset_module_training_mode,
+    _set_module_training_mode,
+)
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+def predict(
+    predict_unit: PredictUnit,
+    dataloader: Iterable[TPredictData],
+    *,
+    max_steps_per_epoch: Optional[int] = None,
+) -> State:
+    """Makes a single pass through the predict dataloader."""
+    state = State(
+        entry_point=EntryPoint.PREDICT,
+        predict_state=PhaseState(
+            dataloader=dataloader,
+            max_steps_per_epoch=max_steps_per_epoch,
+            progress=Progress(),
+        ),
+    )
+    try:
+        _check_loop_condition("max_steps_per_epoch", max_steps_per_epoch)
+        logger.info(f"Started predict with max_steps_per_epoch={max_steps_per_epoch}")
+        _predict_impl(state, predict_unit)
+        logger.info("Finished predict")
+        return state
+    except Exception as e:
+        # TODO: log for diagnostics
+        logger.info(e)
+        predict_unit.on_exception(state, e)
+        raise e
+
+
+@torch.inference_mode()
+def _predict_impl(
+    state: State,
+    predict_unit: PredictUnit,
+) -> None:
+    # Set all modules to eval mode
+    # access modules made available through _AppStateMixin
+    tracked_modules = predict_unit.tracked_modules()
+    prior_module_train_states = _set_module_training_mode(tracked_modules, False)
+
+    predict_unit.on_predict_start(state)
+
+    predict_state = state.predict_state
+    assert predict_state is not None
+
+    # Conditionally run this to avoid running this multiple times
+    # in the case of resuming from a checkpoint mid-epoch
+    if predict_state.progress.num_steps_completed_in_epoch == 0:
+        predict_unit.on_predict_epoch_start(state)
+
+    data_iter = iter(predict_state.dataloader)
+
+    while not _is_epoch_done(predict_state.progress, predict_state.max_steps_per_epoch):
+        try:
+            # TODO: conditionally expose data iterator for use cases that require access during the step
+            batch = next(data_iter)
+            predict_state.step_output = predict_unit.predict_step(state, batch)
+            # clear step_output to avoid retaining extra memory
+            predict_state.step_output = None
+            predict_state.progress.num_steps_completed_in_epoch += 1
+            predict_state.progress.num_steps_completed += 1
+        except StopIteration:
+            break
+    predict_unit.on_predict_epoch_end(state)
+
+    # set progress counters for the next epoch
+    predict_state.progress.num_epochs_completed += 1
+    predict_state.progress.num_steps_completed_in_epoch = 0
+
+    predict_unit.on_predict_end(state)
+
+    # Reset training mode for modules at the end of the epoch
+    # This ensures that side-effects made by the loop are reset before
+    # returning back to the user
+    _reset_module_training_mode(tracked_modules, prior_module_train_states)
