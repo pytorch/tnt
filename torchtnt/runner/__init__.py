df9d88bf26f9b03e619ed7e0318bda8fb12f4537
Danielle Pintz <daniellepintz@fb.com>
2022-09-23 13:43:36 -0700

    Remove engine class (#196)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/196
    
    Removing as we aren't using this for now
    
    Reviewed By: ananthsub
    
    Differential Revision: D39777939
    
    fbshipit-source-id: 2d33a7c042015748a90fc941545ee2e8178ce9c7

--- torchtnt/runner/__init__.py
+++ torchtnt/runner/__init__.py
@@ -4,13 +4,11 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .engine import _Engine
 from .progress import Progress
 from .state import PhaseState, State
 from .unit import EvalUnit, PredictUnit, TrainUnit
 
 __all__ = [
-    "_Engine",
     "Progress",
     "PhaseState",
     "State",
