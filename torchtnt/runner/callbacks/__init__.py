23307df1e4ca500bfa0f7645c82bf1e14ee8b054
Ananth Subramaniam <ananthsub@meta.com>
2022-10-03 18:19:07 -0700

    Rename garbage_collection -> garbage_collector (#216)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/216
    
    As title, rename files
    
    Reviewed By: edward-io
    
    Differential Revision: D40045892
    
    fbshipit-source-id: f570501b27ab34c06b2b7a9b69328fc36c153719

--- torchtnt/runner/callbacks/__init__.py
+++ torchtnt/runner/callbacks/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .garbage_collection import GarbageCollector
+from .garbage_collector import GarbageCollector
 from .pytorch_profiler import PyTorchProfiler
 
 __all__ = ["GarbageCollector", "PyTorchProfiler"]
