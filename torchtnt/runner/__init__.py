7b1640296b3b204c1d959372bba9debb4635ee5f
Danielle Pintz <daniellepintz@fb.com>
2022-10-04 17:00:10 -0700

    Add important classes/APIs to init
    Summary:
    This will make all of these automatically appear in the docs: https://pytorch.org/tnt/torchtnt.runner.html
    Also, users should be able to import these directly from `torchtnt.runner`
    
    Differential Revision: D40081623
    
    fbshipit-source-id: 1956e7d75826fde1625c1f92cfc5469641dd1435

--- torchtnt/runner/__init__.py
+++ torchtnt/runner/__init__.py
@@ -4,14 +4,24 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
+from .callback import Callback
+from .evaluate import evaluate
+from .fit import fit
+from .predict import predict
 from .progress import Progress
 from .state import PhaseState, State
+from .train import train
 from .unit import EvalUnit, PredictUnit, TrainUnit
 
 __all__ = [
+    "Callback",
+    "evaluate",
+    "fit",
+    "predict",
     "Progress",
     "PhaseState",
     "State",
+    "train",
     "EvalUnit",
     "PredictUnit",
     "TrainUnit",
