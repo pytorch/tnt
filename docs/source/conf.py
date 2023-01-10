b014a75532753f17518996c164c3c4092c720917
Jason Senthil <jsenthil@meta.com>
2023-01-10 09:14:22 -0800

    add banner for meta internal docs
    Summary: Add banner to redirect meta employees to internal tnt docs
    
    Reviewed By: daniellepintz
    
    Differential Revision: D42418724
    
    fbshipit-source-id: c9224eda8d62a089570c48f34bab5dab327133fa

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ print(target_dir)
 # -- Project information -----------------------------------------------------
 
 project = "TorchTNT"
-copyright = "2022, Meta"
+copyright = "2023, Meta"
 author = "Meta"
 
 # The full version, including alpha/beta/rc tags
@@ -68,7 +68,7 @@ html_context = {"fbcode": FBCODE}
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path = ["templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -78,12 +78,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "pytorch_sphinx_theme"
-html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
@@ -91,6 +85,33 @@ html_static_path = ["_static"]
 html_theme_options = {
     "display_version": True,
 }
+html_css_files = [
+    "css/torchtnt.css",
+]
+html_js_files = [
+    "js/torchtnt.js",
+]
+
+
+def setup(app):
+    # NOTE: in Sphinx 1.8+ `html_css_files` is an official configuration value
+    # and can be moved outside of this function (and the setup(app) function
+    # can be deleted).
+
+    # In Sphinx 1.8 it was renamed to `add_css_file`, 1.7 and prior it is
+    # `add_stylesheet` (deprecated in 1.8).
+    add_css = getattr(
+        app, "add_css_file", getattr(app, "add_stylesheet", None)
+    )  # noqa B009
+    for css_file in html_css_files:
+        add_css(css_file)
+
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "pytorch_sphinx_theme"
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
 # where to find external docs
 intersphinx_mapping = {
