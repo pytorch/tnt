18cbbeb7f97ffdc8155c041896cd73c3bde5cda9
Danielle Pintz <daniellepintz@meta.com>
2023-01-18 12:35:03 -0800

    Refactor callback.py class (#304)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/304
    
    Make callback.py one class rather than inherit from other classes. This will make the Callback API appear in the docs as currently it does not: https://pytorch.org/tnt/stable/torchtnt.framework.html#torchtnt.framework.Callback
    
    Reviewed By: JKSenthil
    
    Differential Revision: D42550053
    
    fbshipit-source-id: 3cef0ca75a86b0b7eb6088010eab0396af6a36e3

--- torchtnt/framework/callback.py
+++ torchtnt/framework/callback.py
@@ -4,16 +4,18 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-import logging
 from typing import Union
 
 from torchtnt.framework.state import State
 from torchtnt.framework.unit import TEvalUnit, TPredictUnit, TTrainUnit
 
-logger: logging.Logger = logging.getLogger(__name__)
 
+class Callback:
+    @property
+    def name(self) -> str:
+        """Should be a distinct name per instance. This is useful for debugging, profiling, and checkpointing purposes."""
+        return self.__class__.__qualname__
 
-class _OnExceptionMixin:
     def on_exception(
         self,
         state: State,
@@ -22,15 +24,6 @@ class _OnExceptionMixin:
     ) -> None:
         pass
 
-
-class _NameMixin:
-    @property
-    def name(self) -> str:
-        """Should be a distinct name per instance. This is useful for debugging, profiling, and checkpointing purposes."""
-        return self.__class__.__qualname__
-
-
-class _TrainCallback:
     def on_train_start(self, state: State, unit: TTrainUnit) -> None:
         pass
 
@@ -49,8 +42,6 @@ class _TrainCallback:
     def on_train_end(self, state: State, unit: TTrainUnit) -> None:
         pass
 
-
-class _EvalCallback:
     def on_eval_start(self, state: State, unit: TEvalUnit) -> None:
         pass
 
@@ -69,8 +60,6 @@ class _EvalCallback:
     def on_eval_end(self, state: State, unit: TEvalUnit) -> None:
         pass
 
-
-class _PredictCallback:
     def on_predict_start(self, state: State, unit: TPredictUnit) -> None:
         pass
 
@@ -88,9 +77,3 @@ class _PredictCallback:
 
     def on_predict_end(self, state: State, unit: TPredictUnit) -> None:
         pass
-
-
-class Callback(
-    _TrainCallback, _EvalCallback, _PredictCallback, _OnExceptionMixin, _NameMixin
-):
-    pass
