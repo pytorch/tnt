1e8e393683ddc7413413dc01feee0dad3eccbd81
Danielle Pintz <daniellepintz@meta.com>
2023-01-20 15:08:12 -0800

    Add docstring for Callback
    Reviewed By: JKSenthil
    
    Differential Revision: D42644168
    
    fbshipit-source-id: 1f3ba0173fbf4156108eccdcb30ca54a98db1e89

--- torchtnt/framework/callback.py
+++ torchtnt/framework/callback.py
@@ -11,9 +11,48 @@ from torchtnt.framework.unit import TEvalUnit, TPredictUnit, TTrainUnit
 
 
 class Callback:
+    """
+    A Callback is an optional extension that can be used to supplement your loop with additional functionality. Good candidates
+    for such logic are ones that can be re-used across units. To write your own callback, subclass the Callback class and add
+    your own code into the hooks.
+
+    Below is an example of a basic callback which prints a message at various points during execution.
+
+    .. code-block:: python
+
+      from torchtnt.framework import Callback, State, TEvalUnit, TPredictUnit, TTrainUnit
+
+      class PrintingCallback(Callback):
+          def on_train_start(self, state: State, unit: TTrainUnit) -> None:
+              print("Starting training")
+
+          def on_train_end(self, state: State, unit: TTrainUnit) -> None:
+              print("Ending training")
+
+          def on_eval_start(self, state: State, unit: TEvalUnit) -> None:
+              print("Starting evaluation")
+
+          def on_eval_end(self, state: State, unit: TEvalUnit) -> None:
+              print("Ending evaluation")
+
+          def on_predict_start(self, state: State, unit: TPredictUnit) -> None:
+              print("Starting prediction")
+
+          def on_predict_end(self, state: State, unit: TPredictUnit) -> None:
+              print("Ending prediction")
+
+    To use a callback, instantiate the class and pass it in the ``callbacks`` parameter to the :py:func:`~torchtnt.framework.train`, :py:func:`~torchtnt.framework.evaluate`,
+    :py:func:`~torchtnt.framework.predict`, or :py:func:`~torchtnt.framework.fit` entry point.
+
+    .. code-block:: python
+
+      printing_callback = PrintingCallback()
+      train(train_unit, train_dataloader, callbacks=[printing_callback])
+    """
+
     @property
     def name(self) -> str:
-        """Should be a distinct name per instance. This is useful for debugging, profiling, and checkpointing purposes."""
+        """A distinct name per instance. This is useful for debugging, profiling, and checkpointing purposes."""
         return self.__class__.__qualname__
 
     def on_exception(
@@ -22,58 +61,77 @@ class Callback:
         unit: Union[TTrainUnit, TEvalUnit, TPredictUnit],
         exc: BaseException,
     ) -> None:
+        """Hook called when an exception occurs."""
         pass
 
     def on_train_start(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called before training starts."""
         pass
 
     def on_train_epoch_start(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called before a new train epoch starts."""
         pass
 
     def on_train_step_start(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called before a new train step starts."""
         pass
 
     def on_train_step_end(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called after a train step ends."""
         pass
 
     def on_train_epoch_end(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called after a train epoch ends."""
         pass
 
     def on_train_end(self, state: State, unit: TTrainUnit) -> None:
+        """Hook called after training ends."""
         pass
 
     def on_eval_start(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called before evaluation starts."""
         pass
 
     def on_eval_epoch_start(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called before a new eval epoch starts."""
         pass
 
     def on_eval_step_start(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called before a new eval step starts."""
         pass
 
     def on_eval_step_end(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called after an eval step ends."""
         pass
 
     def on_eval_epoch_end(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called after an eval epoch ends."""
         pass
 
     def on_eval_end(self, state: State, unit: TEvalUnit) -> None:
+        """Hook called after evaluation ends."""
         pass
 
     def on_predict_start(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called before prediction starts."""
         pass
 
     def on_predict_epoch_start(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called before a new predict epoch starts."""
         pass
 
     def on_predict_step_start(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called before a new predict step starts."""
         pass
 
     def on_predict_step_end(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called after a predict step ends."""
         pass
 
     def on_predict_epoch_end(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called after a predict epoch ends."""
         pass
 
     def on_predict_end(self, state: State, unit: TPredictUnit) -> None:
+        """Hook called after prediction ends."""
         pass
