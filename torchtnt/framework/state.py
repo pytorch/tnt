213299a36a6879782309cffddf38b261e4fcf5cf
generatedunixname89002005307016 <generatedunixname89002005307016@fb.com>
2023-02-06 15:56:53 -0800

    upgrade pyre version in `fbcode/torchtnt` - batch 1
    Differential Revision: D43043529
    
    fbshipit-source-id: bd6e7ca8d6b533fd8a9c7b024c7340bfe25aea04

--- torchtnt/framework/state.py
+++ torchtnt/framework/state.py
@@ -35,13 +35,9 @@ class EntryPoint(Enum):
     - :py:func:`~torchtnt.framework.predict`
     """
 
-    # pyre-fixme[20]: Argument `value` expected.
     FIT = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     TRAIN = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     EVALUATE = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     PREDICT = auto()
 
 
@@ -59,11 +55,8 @@ class ActivePhase(Enum):
     to determine within which of training, evaluation, or prediction the hook is being called.
     """
 
-    # pyre-fixme[20]: Argument `value` expected.
     TRAIN = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     EVALUATE = auto()
-    # pyre-fixme[20]: Argument `value` expected.
     PREDICT = auto()
 
 
