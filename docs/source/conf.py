3993cab258c2da8ee400228850177586f013ef31
Jason Senthil <jsenthil@meta.com>
2023-03-10 13:44:46 -0800

    Add Examples tab to docs for Meta internal examples (#353)
    Summary: Pull Request resolved: https://github.com/pytorch/tnt/pull/353
    
    Reviewed By: daniellepintz
    
    Differential Revision: D43896216
    
    fbshipit-source-id: 03f987ea7da372a28594cd6fd9ca26eeb31e508f

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,6 +21,7 @@ import os
 import sys
 
 import pytorch_sphinx_theme
+from sphinx.writers.html import HTMLTranslator
 from torchtnt import __version__
 
 sys.path.append(os.path.abspath("./ext"))
@@ -93,6 +94,15 @@ html_js_files = [
 ]
 
 
+class PatchedHTMLTranslator(HTMLTranslator):
+    def visit_reference(self, node):
+        if node.get("newtab") or not (
+            node.get("target") or node.get("internal") or "refuri" not in node
+        ):
+            node["target"] = "_blank"
+        super().visit_reference(node)
+
+
 def setup(app):
     # NOTE: in Sphinx 1.8+ `html_css_files` is an official configuration value
     # and can be moved outside of this function (and the setup(app) function
@@ -105,6 +115,7 @@ def setup(app):
     )  # noqa B009
     for css_file in html_css_files:
         add_css(css_file)
+    app.set_translator("html", PatchedHTMLTranslator)
 
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
