5997b9a6d91b126654aac530674477049f1e077f
Danielle Pintz <daniellepintz@meta.com>
2023-04-18 13:12:19 -0700

    Add data prefetching to AutoUnit (#366)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/366
    
    This runs the host to device data copy in a parallel cuda stream for a performance improvement.
    In order to enable this we need to pass the data iterator to the train_step rather than passing the batch.
    Also removed `_is_last_batch_in_epoch` from utils since it is no longer used
    
    Reviewed By: ananthsub
    
    Differential Revision: D44750106
    
    fbshipit-source-id: 6b012d330a1d2af7c0f36fb5d08cfe1f9d70797f

--- tests/framework/test_train.py
+++ tests/framework/test_train.py
@@ -258,38 +258,6 @@ class TrainTest(unittest.TestCase):
             my_unit.train_step.call_count, max_epochs * expected_steps_per_epoch
         )
 
-    def test_train_is_last_batch(self) -> None:
-        """
-        Test train entry point and the is_last_batch field of train_state
-        """
-        input_dim = 2
-        dataset_len = 8
-        batch_size = 2
-        max_epochs = 3
-        expected_steps_per_epoch = dataset_len / batch_size
-
-        my_unit = LastBatchTrainUnit(
-            input_dim=input_dim, expected_steps_per_epoch=expected_steps_per_epoch
-        )
-        initial_training_mode = my_unit.module.training
-
-        dataloader = generate_random_dataloader(dataset_len, input_dim, batch_size)
-        state = init_train_state(dataloader=dataloader, max_epochs=max_epochs)
-        train(state, my_unit)
-
-        self.assertEqual(state.train_state.progress.num_epochs_completed, max_epochs)
-        self.assertEqual(state.train_state.progress.num_steps_completed_in_epoch, 0)
-        self.assertEqual(
-            state.train_state.progress.num_steps_completed,
-            max_epochs * expected_steps_per_epoch,
-        )
-
-        # step_output should be reset to None
-        self.assertEqual(state.train_state.step_output, None)
-
-        self.assertEqual(my_unit.module.training, initial_training_mode)
-        self.assertEqual(state.entry_point, EntryPoint.TRAIN)
-
 
 Batch = Tuple[torch.Tensor, torch.Tensor]
 
@@ -322,35 +290,4 @@ class StopTrainUnit(TrainUnit[Batch]):
             state.stop()
 
         self.steps_processed += 1
-        return loss, outputs
-
-
-class LastBatchTrainUnit(TrainUnit[Batch]):
-    def __init__(self, input_dim: int, expected_steps_per_epoch: int) -> None:
-        super().__init__()
-        self.expected_steps_per_epoch = expected_steps_per_epoch
-        # initialize module, loss_fn, & optimizer
-        self.module = nn.Linear(input_dim, 2)
-        self.loss_fn = nn.CrossEntropyLoss()
-        self.optimizer = torch.optim.SGD(self.module.parameters(), lr=0.01)
-
-    def train_step(
-        self, state: State, data: Batch
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
-        tc = unittest.TestCase()
-        tc.assertEqual(
-            state.train_state.is_last_batch,
-            state.train_state.progress.num_steps_completed_in_epoch + 1
-            == self.expected_steps_per_epoch,
-        )
-
-        inputs, targets = data
-
-        outputs = self.module(inputs)
-        loss = self.loss_fn(outputs, targets)
-        loss.backward()
-
-        self.optimizer.step()
-        self.optimizer.zero_grad()
-
         return loss, outputs
