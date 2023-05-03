664fec743351fb84b354df6478fe441a8982ec65
Jason Senthil <jsenthil@meta.com>
2023-05-03 15:19:36 -0700

    add fsdp support to torchsnapshot_saver (#387)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/387
    
    Add fsdp checkpointing support in torchsnapshot saver, by retrieving `app_state` and overwriting the tracked optimizers with FSDP wrapped ones.
    
    Reviewed By: ananthsub
    
    Differential Revision: D45418537
    
    fbshipit-source-id: efec0a7da5163eeddc19d4e30d26c8c81e9e4304

--- tests/framework/callbacks/test_torchsnapshot_saver.py
+++ tests/framework/callbacks/test_torchsnapshot_saver.py
@@ -11,20 +11,24 @@ import shutil
 import tempfile
 import time
 import unittest
-from typing import List
+from typing import Any, List, Tuple
 
 import torch
 from torch.distributed import launcher
+from torch.optim.lr_scheduler import ExponentialLR
+from torchtnt.framework import AutoUnit
 
 from torchtnt.framework._test_utils import DummyTrainUnit, generate_random_dataloader
 from torchtnt.framework.callbacks import Lambda, TorchSnapshotSaver
+from torchtnt.framework.state import State
 from torchtnt.framework.train import init_train_state, train
-from torchtnt.utils import get_global_rank
-from torchtnt.utils.env import init_from_env
+from torchtnt.utils import get_global_rank, init_from_env, TLRScheduler
 from torchtnt.utils.test_utils import get_pet_launch_config
 
 
 class TorchSnapshotSaverTest(unittest.TestCase):
+    cuda_available = torch.cuda.is_available()
+
     def test_save_every_n_train_steps(self) -> None:
         input_dim = 2
         dataset_len = 10
@@ -154,6 +158,56 @@ class TorchSnapshotSaverTest(unittest.TestCase):
             if get_global_rank() == 0:
                 shutil.rmtree(temp_dir)  # delete temp directory
 
+    @unittest.skipUnless(
+        torch.distributed.is_available(), reason="Torch distributed is needed to run"
+    )
+    @unittest.skipUnless(
+        condition=cuda_available, reason="This test needs a GPU host to run."
+    )
+    def test_save_restore_fsdp(self) -> None:
+        config = get_pet_launch_config(2)
+        launcher.elastic_launch(config, entrypoint=self._save_restore_fsdp)()
+
+    @staticmethod
+    def _save_restore_fsdp() -> None:
+        input_dim = 2
+        dataset_len = 10
+        batch_size = 2
+        max_epochs = 2
+        save_every_n_epochs = 1
+
+        my_unit = DummyAutoUnit(input_dim=input_dim, strategy="fsdp")
+        dataloader = generate_random_dataloader(dataset_len, input_dim, batch_size)
+        state = init_train_state(dataloader=dataloader, max_epochs=max_epochs)
+
+        if get_global_rank() == 0:
+            temp_dir = tempfile.mkdtemp()
+        else:
+            temp_dir = ""
+
+        snapshot_cb = TorchSnapshotSaver(
+            temp_dir,
+            save_every_n_epochs=save_every_n_epochs,
+        )
+        temp_dir = snapshot_cb.dirpath
+        train(state, my_unit, callbacks=[snapshot_cb])
+
+        tc = unittest.TestCase()
+        try:
+            my_new_unit = DummyAutoUnit(input_dim=input_dim, strategy="fsdp")
+            tc.assertNotEqual(
+                my_new_unit.optimizer.state_dict(), my_unit.optimizer.state_dict()
+            )
+            # get latest checkpoint
+            ckpt_path = os.path.join(temp_dir, f"epoch_{max_epochs}_step_10")
+            snapshot_cb.restore(ckpt_path, state, my_new_unit)
+            tc.assertEqual(
+                my_new_unit.optimizer.state_dict(), my_unit.optimizer.state_dict()
+            )
+        finally:
+            if get_global_rank() == 0:
+                shutil.rmtree(temp_dir)  # delete temp directory
+
     def test_saver_invalid_args(self) -> None:
         with tempfile.TemporaryDirectory() as temp_dir:
             with self.assertRaisesRegex(
@@ -165,3 +219,25 @@ class TorchSnapshotSaverTest(unittest.TestCase):
                 ValueError, "Invalid value passed for save_every_n_epochs.*"
             ):
                 TorchSnapshotSaver(temp_dir, save_every_n_epochs=-2)
+
+
+Batch = Tuple[torch.tensor, torch.tensor]
+
+
+class DummyAutoUnit(AutoUnit[Batch]):
+    def __init__(self, input_dim: int, *args, **kwargs):
+        super().__init__(module=torch.nn.Linear(input_dim, 2), *args, **kwargs)
+
+    def compute_loss(self, state: State, data: Batch) -> Tuple[torch.Tensor, Any]:
+        inputs, targets = data
+        outputs = self.module(inputs)
+        loss = torch.nn.functional.cross_entropy(outputs, targets)
+
+        return loss, outputs
+
+    def configure_optimizers_and_lr_scheduler(
+        self, module: torch.nn.Module
+    ) -> Tuple[torch.optim.Optimizer, TLRScheduler]:
+        my_optimizer = torch.optim.SGD(self.module.parameters(), lr=0.01)
+        my_lr_scheduler = ExponentialLR(my_optimizer, gamma=0.9)
+        return my_optimizer, my_lr_scheduler
