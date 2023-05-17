78f10bace02f488c3aaf2aa1b816ea52f2cc1a8f
Aleksandr Pivovar <apivovar@meta.com>
2023-05-17 07:33:33 -0700

    Added an TorchRec example to the TNT repo
    Summary: Added TorchRec example usage for torchtnt
    
    Differential Revision: D45653709
    
    fbshipit-source-id: e0ef95130d60bc324497833614f39baa2a9e094c

---
+++ examples/torchrec/tests/torchrec_example_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+import uuid
+
+import torch
+from torch.distributed import launcher
+from torchrec.test_utils import skip_if_asan
+
+from ..main import main
+
+
+MIN_NODES = 1
+MAX_NODES = 1
+PROC_PER_NODE = 2
+
+
+class TorchrecExampleTest(unittest.TestCase):
+    @skip_if_asan
+    @unittest.skipIf(
+        not torch.cuda.is_available(),
+        "Skip when CUDA is not available",
+    )
+    def test_torchrec_example(self) -> None:
+        lc = launcher.LaunchConfig(
+            min_nodes=MIN_NODES,
+            max_nodes=MAX_NODES,
+            nproc_per_node=PROC_PER_NODE,
+            run_id=str(uuid.uuid4()),
+            rdzv_backend="c10d",
+            rdzv_endpoint="localhost:0",
+            max_restarts=0,
+            monitor_interval=1,
+        )
+
+        launcher.elastic_launch(config=lc, entrypoint=main)([])
