9d52a15382059c90039432058194be797c7037eb
Athma Sagar <athmasagar@fb.com>
2023-05-30 14:58:14 -0700

    Revert D45170444: add more granular TARGETS in tnt utils
    Differential Revision:
    D45170444
    
    Original commit changeset: 8856e790bb97
    
    Original Phabricator Diff: D45170444
    
    fbshipit-source-id: f43c37d4709c2e230759ece57057cb529680917d

--- torchtnt/utils/env.py
+++ torchtnt/utils/env.py
@@ -9,15 +9,11 @@ import logging
 import os
 import typing as T
 from datetime import timedelta
-from typing import Optional, Union
-
-import numpy as np
 
 import torch
-from numpy import random
 from torch.distributed.constants import default_pg_timeout
+from torchtnt.utils import get_process_group_backend_from_device
 from torchtnt.utils.device import get_device_from_env, maybe_enable_tf32
-from torchtnt.utils.distributed import get_process_group_backend_from_device
 
 _log: logging.Logger = logging.getLogger(__name__)
 
@@ -91,49 +87,3 @@ def init_from_env(
         torch.distributed.init_process_group(backend=pg_backend, timeout=pg_timeout)
     maybe_enable_tf32(float32_matmul_precision)
     return device
-
-
-def seed(seed: int, deterministic: Optional[Union[str, int]] = None) -> None:
-    """Function that sets seed for pseudo-random number generators across commonly used libraries.
-
-    This seeds PyTorch, NumPy, and the python.random module.
-    For more details, see https://pytorch.org/docs/stable/notes/randomness.html.
-
-    Args:
-        seed: the integer value seed.
-        deterministic: Controls determinism settings within PyTorch.
-            If `None`, don't set any PyTorch global values.
-            If "default" or 0, don't error or warn on nondeterministic operations and additionally enable PyTorch CuDNN benchmark.
-            If "warn" or 1, warn on nondeterministic operations and disable PyTorch CuDNN benchmark.
-            If "error" or 2, error on nondeterministic operations and disable PyTorch CuDNN benchmark.
-            For more details, see :func:`torch.set_deterministic_debug_mode` and
-            https://pytorch.org/docs/stable/notes/randomness.html#avoiding-nondeterministic-algorithms.
-
-    Raises:
-        ValueError
-            If the input seed value is outside the required range.
-    """
-    max_val = np.iinfo(np.uint32).max
-    min_val = np.iinfo(np.uint32).min
-    if seed < min_val or seed > max_val:
-        raise ValueError(
-            f"Invalid seed value provided: {seed}. Value must be in the range [{min_val}, {max_val}]"
-        )
-    _log.debug(f"Setting seed to {seed}")
-
-    torch.manual_seed(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-
-    if deterministic is not None:
-        _log.debug(f"Setting deterministic debug mode to {deterministic}")
-        torch.set_deterministic_debug_mode(deterministic)
-        deterministic_debug_mode = torch.get_deterministic_debug_mode()
-        if deterministic_debug_mode == 0:
-            _log.debug("Disabling cuDNN deterministic mode")
-            torch.backends.cudnn.deterministic = False
-            torch.backends.cudnn.benchmark = True
-        else:
-            _log.debug("Enabling cuDNN deterministic mode")
-            torch.backends.cudnn.deterministic = True
-            torch.backends.cudnn.benchmark = False
