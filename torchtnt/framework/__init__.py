f1d0d10a02dc00514872b8bd3b0513245c3e175b
Ananth Subramaniam <ananthsub@meta.com>
2023-06-16 19:12:17 -0700

    Add auto predict unit for batch inference use cases
    Summary: This is a useful accompaniment to the AutoUnit, in that we do not have to define the `compute_loss` or `configure_optimizers_and_lr_schedulers` required by the `AutoUnit` interface. This also adds the prefetching/overlapping host to device with module forward for predictions
    
    Reviewed By: daniellepintz
    
    Differential Revision: D46610847
    
    fbshipit-source-id: 726ae12136ea755309dfc421f6185b28e931aeae

--- torchtnt/framework/__init__.py
+++ torchtnt/framework/__init__.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-from .auto_unit import AutoUnit
+from .auto_unit import AutoPredictUnit, AutoUnit
 from .callback import Callback
 from .evaluate import evaluate, init_eval_state
 from .fit import fit, init_fit_state
@@ -14,6 +14,7 @@ from .train import init_train_state, train
 from .unit import EvalUnit, PredictUnit, TEvalUnit, TPredictUnit, TrainUnit, TTrainUnit
 
 __all__ = [
+    "AutoPredictUnit",
     "AutoUnit",
     "Callback",
     "evaluate",
