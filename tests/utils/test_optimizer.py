4365318d56d05d26e4ab3de85341f5730f915983
Danielle Pintz <daniellepintz@meta.com>
2023-09-27 11:47:09 -0700

    In TorchSnapshotSaver initialize optimizer state before restoring (#554)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/554
    
    Optimizer state is lazily initialized, meaning when you create an optimizer and call state_dict() it will have fewer keys than when you create an optimizer, call optimizer.step() and then call state_dict().
    
    If you create an optimizer and then try to restore the optimizer from a previously saved state_dict() (which was saved after optimizer.step()), it will fail because not all of the keys are in the state_dict() of the initial optimizer.
    
    The workaround is to initialize optimizer state with `optimizer.step()` before restoring.
    
    Reviewed By: ananthsub, janeyx99
    
    Differential Revision: D49563335
    
    fbshipit-source-id: 55e591888e0b891ec69aed2e70578bf041cd8835

---
+++ tests/utils/test_optimizer.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import unittest
+
+import torch
+from torchtnt.utils.env import init_from_env
+from torchtnt.utils.optimizer import init_optim_state
+
+
+class OptimizerTest(unittest.TestCase):
+    def test_init_optim_state(self) -> None:
+        """Test optimizer skeleton state initialization."""
+        device = init_from_env()
+        module = torch.nn.Linear(1, 1, device=device)
+        original_state_dict = module.state_dict().copy()
+        optimizer = torch.optim.AdamW(module.parameters(), lr=0.01)
+        self.assertEqual(optimizer.state, {})
+
+        init_optim_state(optimizer)
+
+        # check that optimizer state has been initialized
+        self.assertNotEqual(optimizer.state, {})
+
+        # check that parameters have not changed
+        self.assertTrue(
+            torch.allclose(
+                original_state_dict["weight"],
+                module.state_dict()["weight"],
+            )
+        )
+        self.assertTrue(
+            torch.allclose(
+                original_state_dict["bias"],
+                module.state_dict()["bias"],
+            )
+        )
