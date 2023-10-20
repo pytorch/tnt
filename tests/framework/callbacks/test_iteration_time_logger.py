30e1d2702be4b34dd54e7ece0596d60b6972eece
Chip Turner <chip@meta.com>
2023-10-20 15:13:42 -0700

    Log eval and prediction iteration times
    Summary:
    In addition to previously logging `train_iteration_time`, now
    also log `eval_iteration_time` and `predict_iteration_time` in
    `IterationTimeLogger`.
    
    Reviewed By: galrotem
    
    Differential Revision: D50435391

--- tests/framework/callbacks/test_iteration_time_logger.py
+++ tests/framework/callbacks/test_iteration_time_logger.py
@@ -6,10 +6,15 @@
 # LICENSE file in the root directory of this source tree.
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import call, MagicMock
 
 from torch.utils.tensorboard import SummaryWriter
-from torchtnt.framework._test_utils import DummyTrainUnit, generate_random_dataloader
+from torchtnt.framework._test_utils import (
+    DummyEvalUnit,
+    DummyPredictUnit,
+    DummyTrainUnit,
+    generate_random_dataloader,
+)
 from torchtnt.framework.callbacks.iteration_time_logger import IterationTimeLogger
 
 from torchtnt.framework.state import State
@@ -22,20 +27,54 @@ class IterationTimeLoggerTest(unittest.TestCase):
         logger = MagicMock(spec=TensorBoardLogger)
         logger.writer = MagicMock(spec=SummaryWriter)
         state = MagicMock(spec=State)
-        state.train_state.iteration_timer.recorded_durations = {
-            "train_iteration_time": [1, 3, 5, 7, 9]
+
+        # Test that the recorded times are tracked separately and that we properly
+        # handle when there are no values for a given metric.
+        recorded_durations = {
+            "train_iteration_time": [1, 3, 5, 7, 9],
+            "eval_iteration_time": [],
+            "predict_iteration_time": [11, 13, 15, 17, 19],
         }
+        state.train_state.iteration_timer.recorded_durations = recorded_durations.copy()
+        state.eval_state.iteration_timer.recorded_durations = recorded_durations.copy()
+        state.predict_state.iteration_timer.recorded_durations = (
+            recorded_durations.copy()
+        )
 
-        my_unit = DummyTrainUnit(input_dim=2)
-        my_unit.train_progress.increment_step()
-        my_unit.train_progress.increment_step()
         callback = IterationTimeLogger(logger=logger, moving_avg_window=4)
-        callback.on_train_step_end(state, my_unit)
 
-        logger.writer.add_scalar.assert_called_with(
-            "Train Iteration Time (seconds)",
-            6,  # the average of the last 4 numbers is 6
-            2,  # after incrementing twice, step should be 2
+        # Set up some dummy units for invoking the callback.
+        train_unit = DummyTrainUnit(input_dim=2)
+        train_unit.train_progress.increment_step()
+        train_unit.train_progress.increment_step()
+
+        eval_unit = DummyEvalUnit(input_dim=2)
+        eval_unit.eval_progress.increment_step()
+        eval_unit.eval_progress.increment_step()
+
+        predict_unit = DummyPredictUnit(input_dim=2)
+        predict_unit.predict_progress.increment_step()
+        predict_unit.predict_progress.increment_step()
+
+        # Invoke the callback for each step type we are tracking iteration time for.
+        callback = IterationTimeLogger(logger=logger, moving_avg_window=4)
+        callback.on_train_step_end(state, train_unit)
+        callback.on_eval_step_end(state, eval_unit)
+        callback.on_predict_step_end(state, predict_unit)
+
+        logger.writer.add_scalar.assert_has_calls(
+            [
+                call(
+                    "Train Iteration Time (seconds)",
+                    6.0,  # the average of the last 4 numbers is 6
+                    2,  # after incrementing twice, step should be 2
+                ),
+                call(
+                    "Prediction Iteration Time (seconds)",
+                    16.0,  # the average of the last 4 numbers is 16
+                    2,  # after incrementing twice, step should be 2
+                ),
+            ]
         )
 
     def test_with_train_epoch(self) -> None:
