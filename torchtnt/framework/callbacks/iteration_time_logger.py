19588877187dcf686d60535de067c1aa80392165
Chip Turner <chip@meta.com>
2023-10-20 15:31:33 -0700

    Log eval and prediction iteration times (#590)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/590
    
    In addition to previously logging `train_iteration_time`, now
    also log `eval_iteration_time` and `predict_iteration_time` in
    `IterationTimeLogger`.
    
    Reviewed By: galrotem
    
    Differential Revision: D50435391
    
    fbshipit-source-id: 6c2a2b1f8e7eab5d57d907e8836082186c8b2d41

--- torchtnt/framework/callbacks/iteration_time_logger.py
+++ torchtnt/framework/callbacks/iteration_time_logger.py
@@ -12,9 +12,10 @@ from torch.utils.tensorboard import SummaryWriter
 
 from torchtnt.framework.callback import Callback
 from torchtnt.framework.state import PhaseState, State
-from torchtnt.framework.unit import TTrainUnit
+from torchtnt.framework.unit import TEvalUnit, TPredictUnit, TTrainUnit
 from torchtnt.utils.distributed import get_global_rank
 from torchtnt.utils.loggers.tensorboard import TensorBoardLogger
+from torchtnt.utils.timer import TimerProtocol
 
 
 class IterationTimeLogger(Callback):
@@ -46,24 +47,64 @@ class IterationTimeLogger(Callback):
         self.moving_avg_window = moving_avg_window
         self.log_every_n_steps = log_every_n_steps
 
-    def on_train_step_end(self, state: State, unit: TTrainUnit) -> None:
-        if self._writer is None:
-            return
+    def _log_step_metrics(
+        self,
+        writer: SummaryWriter,
+        metric_label: str,
+        iteration_timer: TimerProtocol,
+        step_logging_for: int,
+    ) -> None:
+        """
+        Helper function to write a timing log message to writer based on how the class
+        was configured.
 
-        step_logging_for = unit.train_progress.num_steps_completed
+        """
         if step_logging_for % self.log_every_n_steps != 0:
             return
 
-        train_state: PhaseState = none_throws(state.train_state)
-        train_iteration_time_list = none_throws(
-            train_state.iteration_timer
-        ).recorded_durations.get("train_iteration_time", [])
-        if len(train_iteration_time_list) == 0:
+        human_metric_names = {
+            "train_iteration_time": "Train Iteration Time (seconds)",
+            "eval_iteration_time": "Eval Iteration Time (seconds)",
+            "predict_iteration_time": "Prediction Iteration Time (seconds)",
+        }
+
+        time_list = iteration_timer.recorded_durations.get(metric_label, [])
+        if not time_list:
             return
 
-        last_n_values = train_iteration_time_list[-self.moving_avg_window :]
-        none_throws(self._writer).add_scalar(
-            "Train Iteration Time (seconds)",
+        last_n_values = time_list[-self.moving_avg_window :]
+        writer.add_scalar(
+            human_metric_names[metric_label],
             sum(last_n_values) / len(last_n_values),
             step_logging_for,
         )
+
+    def on_train_step_end(self, state: State, unit: TTrainUnit) -> None:
+        timer = none_throws(state.train_state).iteration_timer
+        if writer := self._writer:
+            self._log_step_metrics(
+                writer,
+                "train_iteration_time",
+                timer,
+                unit.train_progress.num_steps_completed,
+            )
+
+    def on_eval_step_end(self, state: State, unit: TEvalUnit) -> None:
+        timer = none_throws(state.eval_state).iteration_timer
+        if writer := self._writer:
+            self._log_step_metrics(
+                writer,
+                "eval_iteration_time",
+                timer,
+                unit.eval_progress.num_steps_completed,
+            )
+
+    def on_predict_step_end(self, state: State, unit: TPredictUnit) -> None:
+        timer = none_throws(state.predict_state).iteration_timer
+        if writer := self._writer:
+            self._log_step_metrics(
+                writer,
+                "predict_iteration_time",
+                timer,
+                unit.predict_progress.num_steps_completed,
+            )
