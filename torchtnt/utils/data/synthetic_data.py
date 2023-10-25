23583c36dbec8d20f6b8148889dcd3a2df84679d
Jason Senthil <jsenthil@meta.com>
2023-10-25 16:17:55 -0700

    upstream AbstractRandomDataset into TNT (#593)
    Summary: Pull Request resolved: https://github.com/pytorch/tnt/pull/593
    
    Differential Revision: D50610515
    
    fbshipit-source-id: b28868e63ce6f3dca404e66fb9177e66b62ac056

---
+++ torchtnt/utils/data/synthetic_data.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from __future__ import annotations
+
+import abc
+import logging
+from dataclasses import dataclass, field
+from typing import Dict
+
+import torch
+from torch.utils.data import Dataset
+from torchtnt.utils.device import get_device_from_env
+
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+@dataclass
+class AbstractRandomDataset(Dataset, abc.ABC):
+    """
+    An abstract base class for random datasets.
+
+    Intended for subclassing, this class provides the framework for implementing
+    custom random datasets. Each subclass should provide
+    a concrete implementation of the `_generate_random_item` method.
+
+    Attributes:
+        size (int, default=100): The total number of items the dataset will contain.
+    """
+
+    size: int = field(default=100)
+
+    def __post_init__(self) -> None:
+        if self.size <= 0:
+            raise ValueError(f"Size must be greater than zero. (Received {self.size})")
+        logger.debug(f"Instantiated {self.__class__.__name__} with {self.size} items")
+
+    def __len__(self) -> int:
+        """
+        Get the total number of items in the dataset.
+
+        Returns:
+            int: Total number of items.
+        """
+        return self.size
+
+    def __getitem__(self, idx: int) -> Dict[str, torch.Tensor]:
+        """
+        Fetch a dataset item by index.
+
+        Args:
+            idx (int): Index of the desired dataset item.
+
+        Returns:
+            Dict[str, torch.Tensor]: Dictionary containing dataset attributes, primarily tensors.
+            The exact keys depend on the implementation of `_generate_random_item`.
+
+        Raises:
+            IndexError: If the provided index is out of valid range.
+        """
+        if 0 <= idx < self.size:
+            return self._generate_random_item()
+
+        raise IndexError(f"Index {idx} out of range [0, {self.size-1}]")
+
+    @abc.abstractmethod
+    def _generate_random_item(self) -> Dict[str, torch.Tensor]:
+        """
+        Abstract method to produce a random dataset item.
+
+        Subclasses must override this to define their specific random item generation.
+
+        Returns:
+            Dict[str, torch.Tensor]: Dictionary containing item attributes and tensors.
+            The exact keys depend on the implementation of `_generate_random_item`.
+        """
+        raise NotImplementedError(
+            "Subclasses of AbstractRandomDataset should implement _generate_random_item."
+        )
+
+
+def generate_random_square_image_tensor(
+    num_channels: int, side_length: int
+) -> torch.Tensor:
+    """
+    Generate a random tensor with the given image dimensions.
+
+    Args:
+        num_channels (int): Number of channels for the random square image.
+        side_length (int): Side length of the random square image.
+
+    Returns:
+        torch.Tensor: Randomly generated tensor with shape [num_channels, side_length, side_length].
+
+    Raises:
+        ValueError: If num_channels or side_length is not greater than zero.
+    """
+    if num_channels <= 0:
+        raise ValueError(
+            f"num_channels must be greater than zero. Received: {num_channels}"
+        )
+    if side_length <= 0:
+        raise ValueError(
+            f"side_length must be greater than zero. Received: {side_length}"
+        )
+
+    return torch.rand(
+        num_channels, side_length, side_length, device=get_device_from_env()
+    )
