7238fc56ef75c2edae47f30331f488a9203759dd
Danielle Pintz <daniellepintz@meta.com>
2023-10-31 07:32:50 -0700

    Add MemorySnapshot callback (#601)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/601
    
    Add a MemorySnapshot callback which uses the MemorySnapshotProfiler
    
    Reviewed By: crassirostris
    
    Differential Revision: D50772587
    
    fbshipit-source-id: 4a336f40af7a26aac844354e952b25940245aab5

---
+++ tests/framework/callbacks/test_memory_snapshot.py
@@ -0,0 +1,51 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import tempfile
+import unittest
+from unittest.mock import MagicMock, Mock
+
+from torchtnt.framework.callbacks.memory_snapshot import MemorySnapshot
+from torchtnt.framework.state import EntryPoint
+
+
+class TestMemorySnapshot(unittest.TestCase):
+    def test_on_train_step_end(self) -> None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            memory_snapshot = MemorySnapshot(
+                output_dir=temp_dir,
+            )
+            memory_snapshot.memory_snapshot_profiler = Mock()
+
+            mock_state, mock_unit = MagicMock(), MagicMock()
+            memory_snapshot.on_train_step_end(mock_state, mock_unit)
+
+            memory_snapshot.memory_snapshot_profiler.step.assert_called_once()
+
+    def test_on_eval_step_end(self) -> None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            memory_snapshot = MemorySnapshot(
+                output_dir=temp_dir,
+            )
+            memory_snapshot.memory_snapshot_profiler = Mock()
+
+            mock_state, mock_unit = MagicMock(), MagicMock()
+            mock_state.entry_point = EntryPoint.EVALUATE
+            memory_snapshot.on_eval_step_end(mock_state, mock_unit)
+
+            memory_snapshot.memory_snapshot_profiler.step.assert_called_once()
+
+    def test_on_predict_step_end(self) -> None:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            memory_snapshot = MemorySnapshot(
+                output_dir=temp_dir,
+            )
+            memory_snapshot.memory_snapshot_profiler = Mock()
+
+            mock_state, mock_unit = MagicMock(), MagicMock()
+            memory_snapshot.on_predict_step_end(mock_state, mock_unit)
+
+            memory_snapshot.memory_snapshot_profiler.step.assert_called_once()
---
+++ torchtnt/framework/callbacks/memory_snapshot.py
@@ -0,0 +1,65 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import logging
+from typing import Optional
+
+from torchtnt.framework.callback import Callback
+from torchtnt.framework.state import EntryPoint, State
+from torchtnt.framework.unit import TEvalUnit, TPredictUnit, TTrainUnit
+from torchtnt.utils.memory_snapshot_profiler import (
+    MemorySnapshotParams,
+    MemorySnapshotProfiler,
+)
+
+logger: logging.Logger = logging.getLogger(__name__)
+
+
+class MemorySnapshot(Callback):
+
+    """
+    A callback for memory snapshot collection during training, saving pickle files to the user-specified directory.
+    Uses `Memory Snapshots <https://zdevito.github.io/2022/08/16/memory-snapshots.html>`.
+
+    Args:
+        output_dir: Directory where to save the memory snapshots.
+        memory_snapshot_params: Instance of MemorySnapshotParams which will be passed to MemorySnapshotProfiler.
+
+    Note: It is recommended to instantiate this callback **as early as possible** in your training/eval/prediction script,
+        ideally before model initialization, to make sure all memory allocation is captured.
+
+    """
+
+    def __init__(
+        self,
+        *,
+        output_dir: str,
+        memory_snapshot_params: Optional[MemorySnapshotParams] = None,
+    ) -> None:
+        self.memory_snapshot_profiler = MemorySnapshotProfiler(
+            output_dir=output_dir, memory_snapshot_params=memory_snapshot_params
+        )
+        self.memory_snapshot_profiler.start()
+
+    def on_train_step_end(self, state: State, unit: TTrainUnit) -> None:
+        self.memory_snapshot_profiler.step()
+
+    def on_train_end(self, state: State, unit: TTrainUnit) -> None:
+        self.memory_snapshot_profiler.stop()
+
+    def on_eval_step_end(self, state: State, unit: TEvalUnit) -> None:
+        self.memory_snapshot_profiler.step()
+
+    def on_eval_end(self, state: State, unit: TEvalUnit) -> None:
+        # if in fit do nothing since the profiler will be stopped in on_train_end
+        if state.entry_point == EntryPoint.EVALUATE:
+            self.memory_snapshot_profiler.stop()
+
+    def on_predict_step_end(self, state: State, unit: TPredictUnit) -> None:
+        self.memory_snapshot_profiler.step()
+
+    def on_predict_end(self, state: State, unit: TPredictUnit) -> None:
+        self.memory_snapshot_profiler.stop()
