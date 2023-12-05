de7bbdbb293c6cfa29df5b0976ef0ba04dcd4669
Jason Senthil <jsenthil@meta.com>
2023-12-05 10:24:11 -0800

    move checkpoint directory utils from TSS into new utils file (#641)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/641
    
    # Context
    We want to share few directory i/o related utils with future checkpointers. Currently most of these sit in TSS
    
    # This Diff
    Moves few utils like `get_latest_checkpoint_path`, `_latest_checkpoint_path`,
    `_retrieve_checkpoint_dirpaths`, and
    `_delete_snapshot` to `_checkpoint_utils.py` and moves unit tests from `test_torchsnapshot_saver.py` to `test_checkpoint_utils.py` accordingly
    
    Also adds `metadata_fname` arg to `_delete_snapshot` and `get_latest_checkpoint_path` since currently these are hardcoded for TorchSnapshot use case
    
    finally renames `_delete_snapshot` to `_delete_checkpoint` to maintain same naming scheme
    
    Reviewed By: galrotem
    
    Differential Revision: D51817876
    
    fbshipit-source-id: d7a0e333f775849790959584e4e10ce92d82fab9

--- torchtnt/framework/callbacks/torchsnapshot_saver.py
+++ torchtnt/framework/callbacks/torchsnapshot_saver.py
@@ -6,26 +6,19 @@
 
 import logging
 import os
-import re
 from contextlib import contextmanager, ExitStack
-from typing import (
-    Any,
-    cast,
-    Dict,
-    Generator,
-    Iterable,
-    List,
-    Optional,
-    Pattern,
-    Set,
-    Union,
-)
+from typing import Any, cast, Dict, Generator, Iterable, List, Optional, Set, Union
 
 import torch.distributed as dist
 
 from pyre_extensions import none_throws
 
 from torchtnt.framework.callback import Callback
+from torchtnt.framework.callbacks._checkpoint_utils import (
+    _delete_checkpoint,
+    _retrieve_checkpoint_dirpaths,
+    get_latest_checkpoint_path,
+)
 from torchtnt.framework.callbacks.checkpointer_types import KnobOptions, RestoreOptions
 from torchtnt.framework.state import EntryPoint, State
 from torchtnt.framework.unit import (
@@ -335,7 +328,7 @@ class TorchSnapshotSaver(Callback):
         with get_timing_context(state, f"{self.__class__.__name__}.delete_snapshot"):
             if self._pg_wrapper.get_rank() == 0:
                 # only delete on rank 0
-                _delete_snapshot(oldest_ckpt_path)
+                _delete_checkpoint(oldest_ckpt_path, SNAPSHOT_METADATA_FNAME)
             self._pg_wrapper.barrier()
 
     def _wait(self) -> None:
@@ -503,7 +496,9 @@ class TorchSnapshotSaver(Callback):
         Returns:
             True if the latest snapshot directory was found and successfully restored, otherwise False.
         """
-        path = get_latest_checkpoint_path(dirpath, process_group=process_group)
+        path = get_latest_checkpoint_path(
+            dirpath, SNAPSHOT_METADATA_FNAME, process_group=process_group
+        )
         if path is None:
             return False
         logger.info(f"Restoring from path: {path}")
@@ -517,108 +512,6 @@ class TorchSnapshotSaver(Callback):
             knob_options=knob_options,
         )
         return True
-
-
-def get_latest_checkpoint_path(
-    dirpath: str, process_group: Optional[dist.ProcessGroup] = None
-) -> Optional[str]:
-    """
-    Given a parent directory where checkpoints are saved, return the latest checkpoint subdirectory.
-
-    Args:
-        dirpath: parent directory where checkpoints are saved.
-        process_group: the process group on which the ranks will communicate on. default: ``None`` (the entire world)
-
-    Raises:
-        AssertionError if the checkpoint subdirectories are not named in the format epoch_{epoch}_step_{step}.
-    """
-
-    ret = None
-    rank = get_global_rank()
-    # Do all filesystem reads from rank 0 only
-    if rank == 0:
-        ret = _latest_checkpoint_path(dirpath)
-
-    # If not running in a distributed setting, return as is
-    if not (dist.is_available() and dist.is_initialized()):
-        return ret
-
-    # Otherwise, broadcast result from rank 0 to all ranks
-    pg = PGWrapper(process_group)
-    path_container = [ret] if rank == 0 else [None]
-    pg.broadcast_object_list(path_container, 0)
-    val = path_container[0]
-    return val
-
-
-def _latest_checkpoint_path(dirpath: str) -> Optional[str]:
-    if dirpath[-1] == "/":
-        # removes trailing forward slash if present
-        # required for regex search to work
-        dirpath = dirpath[:-1]
-
-    fs = get_filesystem(dirpath)
-
-    if not fs.exists(dirpath):
-        logger.warning(f"Input dirpath doesn't exist: {dirpath}")
-        return None
-
-    contents = fs.ls(dirpath, detail=True)
-    contents = [item["name"] for item in contents if item["type"] == "directory"]
-    if len(contents) == 0:
-        logger.warning(f"Input dirpath doesn't contain any subdirectories: {dirpath}")
-        return None
-
-    # Define the regex pattern to match the directory names
-    pattern = rf"^{dirpath}/epoch_\d+_step_\d+"
-    snapshot_dirpath_pattern: Pattern[str] = re.compile(pattern)
-    candidate_dirpaths = list(filter(snapshot_dirpath_pattern.match, contents))
-
-    if len(candidate_dirpaths) == 0:
-        logger.warning(
-            f"No valid checkpoint directories were found in input dirpath: {dirpath}"
-        )
-        return None
-
-    # Initialize variables to store the largest epoch and step numbers
-    largest_subdirectory = None
-    largest_epoch = -1
-    largest_step = -1
-
-    # Iterate through all files and directories in the specified directory
-    for candidate in candidate_dirpaths:
-        dir_contents = fs.ls(candidate, False)
-        if not any(
-            SNAPSHOT_METADATA_FNAME == os.path.basename(f) for f in dir_contents
-        ):
-            logger.warning(
-                f"Snapshot metadata is missing from {candidate}! Skipping this path"
-            )
-            continue
-
-        # Extract the epoch and step numbers from the directory name
-        dirname = os.path.basename(candidate)
-
-        # dirname will be of the format epoch_N_step_M
-        # where N is the epoch number and M is the step number as integers
-        split = dirname.split("_")
-        if len(split) != 4:
-            raise AssertionError(
-                f"Expected exactly 4 elements for pattern of epoch_N_step_M, but received {split})"
-            )
-
-        epoch_num, step_num = int(split[1]), int(split[3])
-        # Check if the current epoch and step numbers are larger than the largest ones found so far
-        if epoch_num > largest_epoch:
-            largest_epoch = epoch_num
-            largest_step = step_num
-            largest_subdirectory = dirname
-        elif largest_epoch == epoch_num and step_num > largest_step:
-            largest_step = step_num
-            largest_subdirectory = dirname
-
-    # Rejoin with the parent directory path and return the largest subdirectory
-    return os.path.join(dirpath, none_throws(largest_subdirectory))
 
 
 def _validate_snapshot_available() -> None:
@@ -691,33 +584,3 @@ def _override_knobs(
         for mgr in knobs:
             stack.enter_context(mgr)
         yield
-
-
-def _retrieve_checkpoint_dirpaths(dirpath: str) -> List[str]:
-    """
-    Given a parent directory where checkpoints are saved, return the sorted checkpoint subdirectories
-    from oldest to newest.
-
-    Args:
-        dirpath: parent directory where checkpoints are saved.
-    """
-    fs = get_filesystem(dirpath)
-
-    contents = fs.ls(dirpath, detail=True)
-    contents = [item["name"] for item in contents if item["type"] == "directory"]
-    ckpt_dirpaths = []
-    for path in contents:
-        match = re.search(r"epoch_(\d+)_step_(\d+)", path)
-        if match:
-            ckpt_dirpaths.append(path)
-
-    # sorts by epoch, then step
-    ckpt_dirpaths.sort(key=lambda x: (int(x.split("_")[1]), int(x.split("_")[3])))
-    return ckpt_dirpaths
-
-
-def _delete_snapshot(dirpath: str) -> None:
-    fs = get_filesystem(dirpath)
-    if not fs.exists(os.path.join(dirpath, ".snapshot_metadata")):
-        raise RuntimeError(f"{dirpath} does not contain .snapshot_metadata")
-    fs.rm(dirpath, recursive=True)
