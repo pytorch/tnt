02b053a3c91d18766850dd3b1a25fda002967ec8
Pyre Bot Jr <generatedunixname89002005307016@meta.com>
2024-03-05 10:06:03 -0800

    upgrade pyre version in `fbcode/torchtnt` - batch 1
    Differential Revision: D54510813

--- tests/utils/loggers/test_csv.py
+++ tests/utils/loggers/test_csv.py
@@ -26,7 +26,6 @@ class CSVLoggerTest(unittest.TestCase):
 
             with open(csv_path) as f:
                 output = list(csv.DictReader(f))
-                # pyre-fixme[16]: `_DictReadMapping` has no attribute `__getitem__`.
                 self.assertEqual(float(output[0][log_name]), log_value)
                 self.assertEqual(int(output[0]["step"]), log_step)
 
@@ -42,6 +41,5 @@ class CSVLoggerTest(unittest.TestCase):
 
             with open(csv_path) as f:
                 output = list(csv.DictReader(f))
-                # pyre-fixme[16]: `_DictReadMapping` has no attribute `__getitem__`.
                 self.assertEqual(float(output[0][log_name]), log_value)
                 self.assertEqual(int(output[0]["step"]), log_step)
