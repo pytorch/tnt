075ce25f3f9c31d7097a3c1241335d36190a16db
Gal Rotem <galrotem@meta.com>
2024-05-21 07:08:02 -0700

    add warmup steps to iteration time logger (#837)
    Summary:
    
    Adding warmup steps to different performance loggers
    
    Differential Revision: D57595957

--- torchtnt/framework/callbacks/iteration_time_logger.py
+++ torchtnt/framework/callbacks/iteration_time_logger.py
@@ -27,19 +27,36 @@ class IterationTimeLogger(Callback):
     Args:
         logger: Either a :class:`torchtnt.loggers.tensorboard.TensorBoardLogger`
             or a :class:`torch.utils.tensorboard.SummaryWriter` instance.
-        moving_avg_window: an optional int to control the moving average window
-        log_every_n_steps: an optional int to control the log frequency
+        moving_avg_window: an int to control the moving average window. Default is 1.
+        log_every_n_steps: an int to control the log frequency. Default is 1.
+        warmup_steps: an int to control the number of warmup steps. We will start logging only after the amount of warmup steps were completed. Default is 0.
     """
 
     def __init__(
         self,
         logger: Union[MetricLogger, SummaryWriter],
+        *,
         moving_avg_window: int = 1,
         log_every_n_steps: int = 1,
+        warmup_steps: int = 0,
     ) -> None:
         self._logger = logger
+
+        if moving_avg_window < 1:
+            raise ValueError(
+                f"moving_avg_window must be at least 1, got {moving_avg_window}"
+            )
         self.moving_avg_window = moving_avg_window
+
+        if log_every_n_steps < 1:
+            raise ValueError(
+                f"log_every_n_steps must be at least 1, got {log_every_n_steps}"
+            )
         self.log_every_n_steps = log_every_n_steps
+
+        if warmup_steps < 0:
+            raise ValueError(f"warmup_steps must be at least 0, got {warmup_steps}")
+        self.warmup_steps = warmup_steps
 
     @rank_zero_fn
     def _log_step_metrics(
@@ -53,6 +70,9 @@ class IterationTimeLogger(Callback):
         was configured.
 
         """
+        if step_logging_for <= self.warmup_steps:
+            return
+
         if step_logging_for % self.log_every_n_steps != 0:
             return
 
