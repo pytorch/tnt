882832c1f5019f4149a2d5d79c2dc35e98fb2a1e
Gal Rotem <galrotem@meta.com>
2024-06-21 15:17:16 -0700

    add eval support to distributed sync (#847)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tnt/pull/847
    
    Support also eval
    
    Reviewed By: diego-urgell
    
    Differential Revision: D58855082
    
    fbshipit-source-id: b2ed61e27d1d8a786ba3352f14437b48097a2e4d

--- tests/framework/callbacks/test_periodic_distributed_sync.py
+++ tests/framework/callbacks/test_periodic_distributed_sync.py
@@ -10,7 +10,7 @@
 import unittest
 from unittest.mock import MagicMock, patch
 
-from torchtnt.framework._test_utils import DummyPredictUnit
+from torchtnt.framework._test_utils import DummyEvalUnit, DummyPredictUnit
 
 from torchtnt.framework.callbacks.periodic_distributed_sync import (
     PeriodicDistributedSync,
@@ -20,7 +20,7 @@ from torchtnt.framework.state import EntryPoint, State
 
 class PeriodicDistributedSyncTest(unittest.TestCase):
     @patch("torchtnt.framework.callbacks.periodic_distributed_sync.barrier")
-    def test_frequency(self, barrier_mock: MagicMock) -> None:
+    def test_frequency_predict(self, barrier_mock: MagicMock) -> None:
         pds = PeriodicDistributedSync(sync_every_n_steps=2)
         unit = DummyPredictUnit(2)
         state = State(entry_point=EntryPoint.PREDICT)
@@ -30,4 +30,17 @@ class PeriodicDistributedSyncTest(unittest.TestCase):
 
         unit.predict_progress.increment_step()  # 2 steps completed
         pds.on_predict_step_end(state, unit)
+        barrier_mock.assert_called_once()
+
+    @patch("torchtnt.framework.callbacks.periodic_distributed_sync.barrier")
+    def test_frequency_evaluate(self, barrier_mock: MagicMock) -> None:
+        pds = PeriodicDistributedSync(sync_every_n_steps=2)
+        unit = DummyEvalUnit(2)
+        state = State(entry_point=EntryPoint.EVALUATE)
+        unit.eval_progress.increment_step()  # 1 step completed
+        pds.on_eval_step_end(state, unit)
+        barrier_mock.assert_not_called()
+
+        unit.eval_progress.increment_step()  # 2 steps completed
+        pds.on_eval_step_end(state, unit)
         barrier_mock.assert_called_once()
