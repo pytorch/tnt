9bf5d6a13a6bb226648b87a8349a2a9c1bcd7a5e
Jason Senthil <jsenthil@meta.com>
2024-11-08 15:05:31 -0800

    add event + event handlers for custom logging
    Reviewed By: diego-urgell
    
    Differential Revision: D65491587

---
+++ torchtnt/utils/event.py
@@ -0,0 +1,25 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass, field
+from typing import Dict, Union
+
+EventMetadataValue = Union[str, int, float, bool, None]
+
+
+@dataclass
+class Event:
+    """
+    The class represents the generic event that occurs during a TorchTNT
+    loop. The event can be any kind of meaningful action.
+
+    Args:
+        name: event name.
+        metadata: additional data that is associated with the event.
+    """
+
+    name: str
+    metadata: Dict[str, EventMetadataValue] = field(default_factory=dict)
