Updated in broadcast_str to use correct tensor size
Summary:
Tensor size from source rank was [] before the fix while on other ranks tensor size was [1]. Broadcasting from [] to [1] is an illegal usage.
The bug heppened to not cause any failures.

Reviewed By: diego-urgell, JKSenthil, fduwjj

Differential Revision: D77901586

fbshipit-source-id: 2be33d7a2fcd1113995fcff499b6e90b06c0abb3

torchtnt/utils/distributed.py
@@ -738,7 +738,7 @@ def broadcast_str(
         # convert string to tensor
         buffer = torch.frombuffer(val.encode("utf-8"), dtype=torch.uint8)
         buffer = buffer.to(device=device)
-        buffer_length = torch.tensor((len(buffer)), dtype=torch.int, device=device)
+        buffer_length = torch.tensor([len(buffer)], dtype=torch.int, device=device)
 
         if fixed_buffer_size is not None:
             if len(buffer) > fixed_buffer_size: